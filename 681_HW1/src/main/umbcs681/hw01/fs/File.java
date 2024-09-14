a413b6acbe77d9af1854f9fc10e7b747c577459d
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-09-13 22:26:49 -0400

    ASSIGNMENT_1

---
+++ 681_HW1/src/main/umbcs681/hw01/fs/File.java
@@ -0,0 +1,35 @@
+package umbcs681.hw01.fs;
+
+import java.time.LocalDateTime;
+
+import umbcs681.hw01.util.FSVisitor;
+
+public class File extends FSElement {
+
+    public File(Directory parent, String name, int size, LocalDateTime creationTime) {
+        super(parent, name, size, creationTime);
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+
+    @Override
+    public String getPath() {
+        StringBuilder path = new StringBuilder(name);
+        Directory currentParent = parent;
+
+        while (currentParent != null) {
+            path.insert(0, currentParent.getName() + "/");
+            currentParent = currentParent.getParent();
+        }
+
+        return path.toString();
+    }
+
+    @Override
+    public void accept(FSVisitor visitor) {
+        visitor.visit(this);
+    }
+}
