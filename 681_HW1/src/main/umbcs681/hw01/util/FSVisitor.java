06186a9d7c045e4acfd362dabac4c66c541d1ac1
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-09-24 15:52:53 -0400

    Delete 681_HW1/src directory

--- 681_HW1/src/main/umbcs681/hw01/util/FSVisitor.java
+++
@@ -1,9 +0,0 @@
-package umbcs681.hw01.util;
-
-import umbcs681.hw01.fs.*;
-
-public interface FSVisitor {
-    void visit(Link link);
-    void visit(Directory directory);
-    void visit(File file);
-}
