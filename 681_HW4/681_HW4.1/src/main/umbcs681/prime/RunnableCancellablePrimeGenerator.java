b15b473cac5dc1c171f0c2ef13b5ad42c9fbe930
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-11-04 20:16:28 -0500

    hw_4

---
+++ 681_HW4/681_HW4.1/src/main/umbcs681/prime/RunnableCancellablePrimeGenerator.java
@@ -0,0 +1,60 @@
+package umbcs681.prime;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RunnableCancellablePrimeGenerator extends RunnablePrimeGenerator {
+    private boolean done = false;
+    private final ReentrantLock lock = new ReentrantLock();
+    
+    public RunnableCancellablePrimeGenerator(long from, long to) {
+        super(from, to);
+    }
+    
+    public void setDone() {
+        lock.lock();
+        try {
+            done = true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void generatePrimes() {
+        for (long n = from; n <= to; n++) {
+            lock.lock();
+            try {
+                if (done) {
+                    System.out.println("Stopped generating prime numbers.");
+                    this.primes.clear();
+                    break;
+                }
+            } finally {
+                lock.unlock();
+            }
+
+            if (isPrime(n)) {
+                lock.lock();
+                try {
+                    this.primes.add(n);
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RunnableCancellablePrimeGenerator gen = new RunnableCancellablePrimeGenerator(1, 100);
+        Thread thread = new Thread(gen);
+        thread.start();
+        gen.setDone();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        gen.getPrimes().forEach((Long prime) -> System.out.print(prime + ", "));
+        System.out.println("\n" + gen.getPrimes().size() + " prime numbers are found.");
+    }
+}
+
